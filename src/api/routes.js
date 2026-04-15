c2cbe7d4eb7d7dc231c4d51863389ba00d49aee7
KennyMartinHoliem <Kennyholiem07@gmail.com>
2026-04-15 23:19:16 +0700

    setup voucher model, routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
+// controller
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
+const vouchersController = require('./components/vouchers/vouchers-controller');
 
+// wadah yang diexport
 module.exports = () => {
   const app = express.Router();
 
   books(app);
   users(app);
+
+  app.get('/vouchers', vouchersController.getVouchers);
 
   return app;
 };
