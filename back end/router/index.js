b0bf8f3b7f670383a12e28e951d1211515a1d658
IvanOzoraNelsonKho <vaannn1806@gmail.com>
2026-04-18 14:11:41 +0700

    Delete back end/router directory

--- back end/router/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const router = require('./router');
-
-const port = 3000;
-const app = http.createServer(router);
-
-app.listen(port);
-console.log(`Server running on port number: ${port}`);
