22e8f4625be6fb7e2d4530ce4875e41355cf3a28
andrian hartono <andrian.535250057@stu.ac.id>
2026-04-18 15:07:15 +0700

    andrian

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const authRoutes = require('./routes/authRoutes');
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
 const wallet = require('./components/wallet/wallet-route');
@@ -12,6 +12,6 @@ module.exports = () => {
   users(app);
   wallet(app);
   attendance(app);
-
+  app.use('/auth', authRoutes);
   return app;
 };
